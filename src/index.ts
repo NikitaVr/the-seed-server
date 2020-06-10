ac3bbb81d59fba83be7b1659995cdf4a1e057174
V. Andrew <v.andrew@live.com>
2020-06-09 20:06:57 -0400

    Server reset

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,33 @@
-import { World } from './model/world';
-import express from 'express'
+require('dotenv').config();
+import moment from 'moment';
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws'
+
+const config = {
+  port: Number(process.env['port']) | 8080,
+  interval: 2000,
+}
 const app = express();
-import { Server } from 'http'
-const http = new Server(app);
-import socketIO from 'socket.io'
-const ioServer = socketIO(http);
-import { Player, Players, PlayerState } from './model/player';
-import { Direction } from './lib/types';
-import { compareKeys } from './lib/utils';
+const server = http.createServer(app);
 
-require('dotenv').config()
+const wsServer = new WebSocket.Server({server})
 
-const port = process.env.PORT || 3000;
+wsServer.on('connection', (ws) => {
+  ws.on('message', (inMsg) => {
+    console.info(`>in ${moment().toISOString()} ${JSON.stringify(inMsg)}`);
+  })
 
-const mapSize = 100;
-const radius = 4
-const rowSize = 9;
+  const msgInterval = setInterval(() => {
+    const outMsg = JSON.stringify({ time: moment().toISOString() })
+    ws.send(outMsg);
+  }, config.interval);
 
+  ws.on('close', () => {
+    clearInterval(msgInterval);   
+  })
+})
 
-const map = new World(mapSize)
-
-const players: Players = {}
-
-let actions = {}
-
-function getPlayerVision(key: string) {
-    const player = getPlayer(key)
-    const result = map.getVisibleArea(player.state.coords, radius)
-    return result
-}
-
-function getPlayer(key: string) {
-    return players[key]
-}
-
-function getPlayerCoords(key: string) {
-    return getPlayer(key).state.coords
-}
-
-
-// make an item class ? 
-/*
-
-{
-    name: "berries",
-    effect: function(playerKey???)...
-}
-
-*/
-
-
-function processActions() {
-    console.log('Processing Actions');
-    console.log(actions)
-    for (var key in actions) {
-        const player = getPlayer(key)
-        player.state.food -= 1;
-        switch (actions[key].type) {
-            case "move":
-                switch (actions[key].direction) {
-                    case "right":
-                        console.log("MOVE RIGHT")
-                        map.move(player, Direction.Right)
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case "use ground item":
-                map.useResource(player)
-                break;
-            default:
-                break;
-        }
-    }
-    actions = {}
-    //io.emit('get proximity', world);
-    for (var key in players) {
-        if (getPlayer(key).state.food <= 0) {
-            players[key].socket.emit('dead of hunger')
-            players[key].socket.disconnect(true)
-            map.delete(players[key])
-            delete players[key] // make this whole death thing into separate function? with different reasons for death?
-            console.log("DEAD")
-        } else {
-            players[key].socket.emit('get proximity', getPlayerVision(key))
-        }
-
-    }
-}
-
-if (process.env.WAIT_FOR_PLAYERS == 'false') {
-    setInterval(processActions, parseInt(process.env.STEP_TIME) || 1000); // process actions every STEP_TIME, default to 1 second
-}
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-ioServer.on('connection', function (socket: any) {
-    const player = new Player(socket, new PlayerState({ x: 20, y: 20 }, 10))
-    players[player.id] = player
-    map.place(player)
-    socket.on('action', function (action) {
-        actions[player.id] = action
-        console.log(actions)
-
-        if (process.env.WAIT_FOR_PLAYERS == 'true') {
-            // compare if all players have submitted actions
-            if (compareKeys(players, actions)) {
-                processActions()
-            }
-        }
-
-    });
-    player.socket.emit('get proximity', getPlayerVision(player.id))
-});
-
-http.listen(port, function () {
-    console.log('listening on *:' + port);
+server.listen(config.port, () => {
+  console.log(`Server started on port ${config.port}`);
 });
