893f971de8bf203708cdec0ad28df87d63da211a
V. Andrew <v.andrew@live.com>
2020-06-09 22:35:40 -0400

    Make it work

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,21 +1,21 @@
-import { PlayerState } from './../model/player';
-export class TileState{
-    player?: PlayerState
-    berries?: number
-}
+// import { PlayerState } from './../model/player';
+// export class TileState{
+//     player?: PlayerState
+//     berries?: number
+// }
 
 export interface Hash<T = any> {
     [key: string]: T
 }
 
-export interface Coords {
-    x: number
-    y: number
-}
+// export interface Coords {
+//     x: number
+//     y: number
+// }
 
-export enum Direction {
-    Up = 1,
-    Down,
-    Left,
-    Right,
-}
+// export enum Direction {
+//     Up = 1,
+//     Down,
+//     Left,
+//     Right,
+// }
